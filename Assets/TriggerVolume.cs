e80ac18ad947af0c5005c9a281144245f96a36a5
David Messinger <dmessinger03@gmail.com>
2024-04-09 18:01:52 -0500

    Catching Collider Setup

--- Assets/TriggerVolume.cs
+++ Assets/TriggerVolume.cs
@@ -7,8 +7,17 @@ public class TriggerVolume : MonoBehaviour
 {
     public UnityEvent OnEnterTrigger;
 
+    private Collider _collider;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+        _collider.isTrigger = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Entered");
         OnEnterTrigger.Invoke();
     }
 }

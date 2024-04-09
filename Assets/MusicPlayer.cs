e3a67ba0d5520c71743523849f22620adddeebbc
David Messinger <dmessinger03@gmail.com>
2024-04-09 18:12:37 -0500

    MusicPlayer and MusicPlayer Script Setup

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequiredComponent(typeof(AudioSource))]
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private AudioSource _audioSource;
+
+    private void Awake()
     {
-        
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true
+        _audioSource.spatialBlend = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void playMusic(AudioClip clip)
     {
-        
+        _audioSource.clip = clip;
+        _audioSource.Play();
     }
 }
